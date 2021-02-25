0dd235533c6fffb02cc4829060e03e9aa6812084
Jean Felipe <jeanfelipe214@hotmail.com>
2021-02-25 17:28:03 -0300

    Alteraçoes esteticas

--- AlgoritmoKruskal.h
+++ AlgoritmoKruskal.h
@@ -10,7 +10,7 @@ using namespace std;
 
 class AlgoritmoKruskal {
 public:
-    static Graph* findAGMKruskal(Graph *grafo); // encontra a arvore geradora minima usando kruskal
-    static bool ehCiclo(Node *vertice1, Node *vertice2); // confere se o vertice 1 e 2 passado por parametro geram ciclo
-    static void imprimeSolucao(Graph *grafo); // imprime soluçao da agm caso a pessoa queira
+    static Graph* findAGMKruskal(Graph* grafo); // encontra a arvore geradora minima usando kruskal
+    static bool ehCiclo(Node* vertice1, Node* vertice2); // confere se o vertice 1 e 2 passado por parametro geram ciclo
+    static void imprimeSolucao(Graph* grafo); // imprime soluçao da agm caso a pessoa queira
 };
